c5aab724ab49e1620a4432c01bc0774991646565
Gregor Cox <gregorcox@gmail.com>
2024-09-04 14:01:02 +0100

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import "./App.css";
 import Consultation from "./Consultation";
+import logo from "./logo.png";
 
 function App() {
   return (
     <div>
+      <header className="p-4 border-b-2 border-blue-500">
+        <img alt="med express logo" src={logo} width={200} />
+      </header>
+
       <Consultation />
     </div>
   );
